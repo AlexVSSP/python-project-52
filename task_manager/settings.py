eb9f7620b2bb7e182cf509351004087a2ca66265
Aleksei Seleznev <ferfa2@rambler.ru>
2023-02-24 10:18:32 +0300

    add allowed_hosts

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['webserver', '127.0.0.1', '.railway.app']
+ALLOWED_HOSTS = ['webserver', '127.0.0.1', '0.0.0.0', '.railway.app']
 
 
 # Application definition
