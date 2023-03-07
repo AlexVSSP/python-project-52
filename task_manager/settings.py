6156d5de034b4f4e3883b6bb757213241c67fc50
Aleksei Seleznev <ferfa2@rambler.ru>
2023-03-07 21:50:25 +0300

    add localization

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -134,9 +134,15 @@ LOCALE_PATHS = ( os.path.join(BASE_DIR, 'locale'), )
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+
+AUTH_USER_MODEL = 'task_manager.User'
