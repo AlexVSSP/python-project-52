54fa28f2f3d75e7afd928809e36f7a1db91ffad0
Aleksei Seleznev <ferfa2@rambler.ru>
2023-03-09 20:53:13 +0300

    add functionality for authorized users

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 # from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.views import LoginView, LogoutView
@@ -43,18 +45,58 @@ class UserLogoutView(SuccessMessageMixin, LogoutView):
         return super().dispatch(request, *args, **kwargs)
 
 
-class UserUpdateView(SuccessMessageMixin, UpdateView):
+class UserUpdateView(LoginRequiredMixin, UserPassesTestMixin, SuccessMessageMixin, UpdateView):
     model = User
     form_class = UserForm
     template_name = 'update.html'
     success_url = reverse_lazy('users_list')
     success_message = 'Пользователь успешно изменен'
     extra_context = {'button_name': 'Изменить'}
+    login_url = reverse_lazy('user_login')
+
+    def test_func(self):
+        user = self.get_object()
+        return self.request.user.id == user.id
+
+    def handle_no_permission(self):
+        if self.request.user.is_authenticated:
+            url = reverse_lazy('users_list')
+            message = 'У вас недостаточно прав, что редактировать другого пользователя'
+        else:
+            url = self.login_url
+            message = 'Вы не авторизованы! Пожалуйста, выполните вход.'
+        messages.warning(self.request, message)
+        return redirect(url)
+
+    def form_valid(self, form):
+        form.save()
+        username = self.request.POST['username']
+        password = self.request.POST['password1']
+        user = authenticate(self.request, username=username, password=password)
+        login(self.request, user)
+        messages.success(self.request, 'Пользователь успешно изменен')
+        return redirect(self.success_url)
 
 
-class UserDestroyView(SuccessMessageMixin, DeleteView):
+class UserDestroyView(LoginRequiredMixin, UserPassesTestMixin, SuccessMessageMixin, DeleteView):
     model = User
     template_name = 'delete.html'
     success_url = reverse_lazy('users_list')
     success_message = 'Пользователь успешно удален'
     extra_context = {'button_name': 'Да, удалить'}
+    login_url = reverse_lazy('user_login')
+
+
+    def test_func(self):
+        user = self.get_object()
+        return self.request.user.id == user.id
+
+    def handle_no_permission(self):
+        if self.request.user.is_authenticated:
+            url = reverse_lazy('users_list')
+            message = 'У вас недостаточно прав, что редактировать другого пользователя'
+        else:
+            url = self.login_url
+            message = 'Вы не авторизованы! Пожалуйста, выполните вход.'
+        messages.warning(self.request, message)
+        return redirect(url)
