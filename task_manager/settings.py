6f42a5d6f89dab809fe7586193f281b5a8e69df3
Aleksei Seleznev <ferfa2@rambler.ru>
2023-03-15 21:43:08 +0300

    add dj-database-url in settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+import dj_database_url
 import warnings
 
 
@@ -92,6 +93,8 @@ DATABASES = {
     }
 }
 
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
