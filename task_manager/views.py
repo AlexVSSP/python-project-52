3e9eba78ce01e56f54fe693dcfb5ffc7e31bb5c3
Aleksei Seleznev <ferfa2@rambler.ru>
2023-04-10 20:11:51 +0300

    rename several classes

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,5 +1,5 @@
 # from django.contrib.auth import authenticate, login
-from django.shortcuts import render
+# from django.shortcuts import render
 # from django.views import View
 # from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.messages.views import SuccessMessageMixin
@@ -7,20 +7,39 @@ from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.views import LoginView, LogoutView
 # from django.urls import reverse_lazy
 from django.contrib import messages
+from django.utils.translation import gettext as _
+from django.views.generic import TemplateView
+
+# def index(request):
+#     """
+#     Class of displaying the application's home page.
+#     """
+#     return render(request, 'index.html')
 
 
-def index(request):
-    return render(request, 'index.html')
+class IndexView(TemplateView):
+    """
+    Class of displaying the application's home page.
+    """
+    template_name = 'index.html'
 
 
 class UserLoginView(SuccessMessageMixin, LoginView):
+    """
+    Class of displaying the Login page.
+    """
     template_name = 'login.html'
-    success_message = 'Вы залогинены'
-    extra_context = {'button_name': 'Войти'}
+    # success_message = 'Вы залогинены'
+    success_message = _('You are logged in')
+    # extra_context = {'button_name': 'Войти'}
+    extra_context = {'button_name': _('Enter')}
 
 
 class UserLogoutView(SuccessMessageMixin, LogoutView):
-
+    """
+    Class of displaying the Logout page.
+    """
     def dispatch(self, request, *args, **kwargs):
-        messages.add_message(request, messages.INFO, 'Вы разлогинены.')
+        # messages.add_message(request, messages.INFO, 'Вы разлогинены')
+        messages.add_message(request, messages.INFO, _('You are logged out'))
         return super().dispatch(request, *args, **kwargs)
