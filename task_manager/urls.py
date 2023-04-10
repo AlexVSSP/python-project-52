4f5712a19b4309ab4c1cceb32b5d71bc49338be9
Aleksei Seleznev <ferfa2@rambler.ru>
2023-04-10 20:12:48 +0300

    add urls for the website pages

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -16,12 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from task_manager.views import index, UserLoginView, UserLogoutView
+from task_manager.views import IndexView, UserLoginView, UserLogoutView
 
 urlpatterns = [
-    path('', index, name='home'),
+    path('', IndexView.as_view(), name='home'),
     path('admin/', admin.site.urls),
     path('users/', include('task_manager.users.urls')),
+    path('statuses/', include('task_manager.statuses.urls')),
+    path('tasks/', include('task_manager.tasks.urls')),
+    path('labels/', include('task_manager.labels.urls')),
     path('login/', UserLoginView.as_view(), name='user_login'),
     path('logout/', UserLogoutView.as_view(), name='user_logout'),
 ]
